dc22adc99f81e69bc06fcfacfc0fb8a764603ae0
Sijil K P <69392636+kpsijil2@users.noreply.github.com>
2022-01-31 23:50:50 +0530

    Add files via upload

---
+++ request.py
@@ -0,0 +1,7 @@
+import requests
+
+url = 'https://localhost:5000/predict_api'
+s = requests.post(url, json={'HomeTeam': 22, 'AwayTeam': 11, 'HTHG': 2, 'HTAG': 1, 'HS': 13, 'AS': 14, 'HST': 3, 'AST': 2, 'HC': 7,
+                             'AC': 4, 'HR': 0, 'AR': 0})
+
+print(s.json())
